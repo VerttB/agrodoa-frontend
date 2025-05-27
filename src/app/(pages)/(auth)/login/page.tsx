203bdeb1a36a49311db0878931bf026f18b7515b
verttb <alyssonoliveira456@gmail.com>
2025-05-27 18:09:58 -0300

    home page quase pronta

--- src/app/(pages)/(auth)/login/page.tsx
+++ src/app/(pages)/(auth)/login/page.tsx
@@ -81,6 +81,7 @@ export default function Login() {
           Entrar com Google
         </Button>
       </div>
+        
     </form>
   );
 }
---
+++ src/components/Card/index.tsx
@@ -0,0 +1,12 @@
+import { CardContent } from "./CardContent";
+import { CardImage } from "./CardImage";
+import { CardRoot } from "./CardRoot";
+import { CardActions } from "./CardActions";
+
+export const Card = {
+    Root: CardRoot,
+    Image: CardImage,
+    Content: CardContent,
+    Actions : CardActions    
+}
+
