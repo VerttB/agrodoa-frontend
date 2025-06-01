bea9d8f6b15d6ddea69980a9df1ea27cea7b57ce
verttb <alyssonoliveira456@gmail.com>
2025-06-01 12:33:29 -0300

    Login quase pronto

--- src/app/(pages)/(auth)/login/page.tsx
+++ src/app/(pages)/(auth)/login/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import  Input  from "@/components/input";
 import Button from "@/components/button";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Image from "next/image";
@@ -29,12 +30,9 @@ export default function Login() {
     console.log(data);
   };
   return (
-    <div className="flex h-screen justify-center">
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex w-1/4 flex-col items-center justify-center gap-4 self-center rounded-md bg-white p-6 md:w-2/5"
-      >
-        <div className="flex flex-col items-center gap-2 self-center">
+    <div className="flex h-screen justify-center items-center bg-[url(/backgroundAuth.jpg)] bg-cover ">
+      <div className=" flex flex-col justify-around h-3/4 w-2/5 bg-white/60  rounded-br-[100px] rounded-tl-[100px] shadow-2xl backdrop-opacity-60 backdrop-blur-2xl">
+         <div className="flex justify-center items-center gap-2 self-center border-b-1 w-full h-1/5 ">
           <Image
             src="/logo.png"
             width={64}
@@ -43,46 +41,48 @@ export default function Login() {
           ></Image>
           <h1 className="font-sofia"> Agrodoa</h1>
         </div>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex w-full  flex-col h-full items-center  gap-6 self-center rounded-md p-4"
+      >
+       
         <div className="flex w-2/3 flex-col gap-1">
           <h3 className="text-2xl font-medium">Login</h3>
           <p>Preencha os campos abaixo para acessar sua conta</p>
         </div>
         <div className="w-2/3">
-          <label className="flex flex-col gap-1" htmlFor="email">
-            Email
-            <input
-              className="border-secondary-light border-2 border-solid px-2 py-1"
-              type="text"
-              {...register("email")}
-              placeholder="Insira seu email"
-            />
-            {errors && <span> {errors.email?.message} </span>}
-          </label>
-          <label className="flex flex-col gap-1" htmlFor="senha">
-            Senha
-            <input
-              className="border-secondary-light border-2 border-solid px-2 py-1"
-              type="text"
-              {...register("senha")}
-              id="senha"
-              placeholder="Insira sua senha"
-            />
-            {errors && <span> {errors.senha?.message}</span>}
-            <p className="text-sm">Esqueceu a senha?</p>
-          </label>
-          <div className="flex flex-col gap-2">
-            <Link className="text-sm text-blue-400" href={"/cadastro"}>
+          <Input
+                label="Email"
+                type="text"
+                id="email"
+                {...register("email")}
+                placeholder="Insira seu email"
+                errors={errors.email?.message}
+                className="py-2 bg-white"/> 
+          <Input 
+                type="text"
+                id="senha"
+                {...register("senha")}
+                label="Senha" 
+                errors={errors.senha?.message}
+                placeholder="Insira sua senha"
+                className="py-2 bg-white"
+                />
+
+          <div className="flex flex-col gap-2 mt-4">
+            <Link className="text-sm text-blue-500" href={"/cadastro"}>
               Não possui conta? Cadastre-se aqui
             </Link>
-            <Button className="py-1" variant="primary" type="submit">
+            <Button className="py-1 rounded-2xl" variant="primary" type="submit">
               Entrar
             </Button>
-            <Button className="py-1" variant="outlined">
+            <Button className="py-1 rounded-4xl" variant="outlined">
               Entrar com Google
             </Button>
           </div>
         </div>
       </form>
+      </div>
     </div>
   );
 }
