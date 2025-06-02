74a090175ff1849a4ef0604dea254518eabd9b12
VerttB <alyssonoliveira456@gmail.com>
2025-06-02 07:51:25 -0300

    user provider

---
+++ src/providers/UserProvider.tsx
@@ -0,0 +1,15 @@
+import { User } from "@/interfaces/IUser";
+import {  createContext, useState } from "react";
+
+const userContext = createContext<User | null>(null);
+
+export default function UserProvider(children : React.ReactNode){
+
+    const [user, setUser] = useState<User | null>(null)
+
+    return(
+        <userContext.Provider value={user}>
+            {children}
+        </userContext.Provider>
+    )
+}
