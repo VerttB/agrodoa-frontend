9168f8f1a4b5749e820e1b4ae5d2a564d88e4310
VerttB <alyssonoliveira456@gmail.com>
2025-06-02 09:07:16 -0300

    not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+      <div className="text-center p-10">
+        <h1 className="text-4xl font-bold text-red-500">404</h1>
+        <p className="mt-4">Página não encontrada.</p>
+      </div>
+    );
+  }
