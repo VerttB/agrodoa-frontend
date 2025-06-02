11404fe27a4bbc8ad1b4712480c981cb80698155
VerttB <alyssonoliveira456@gmail.com>
2025-06-02 09:31:23 -0300

    mudanças no user context

--- src/providers/UserProvider.tsx
+++ src/providers/UserProvider.tsx
@@ -1,19 +1,37 @@
 'use client'
-import { User } from "@/interfaces/IUser";
-import {  createContext, useState } from "react";
 
-const userContext = createContext<User | null>(null);
+import { User } from "@/interfaces/IUser";
+import { createContext, useState, useContext } from "react";
+
+// Tipo correto do contexto
+type UserContextType = {
+  user: User | null;
+  applyUser: (u: User) => void;
+};
+
+export const userContext = createContext<UserContextType | null>(null);
+export function useUserContext() {
+  const context = useContext(userContext);
+  if (!context) {
+    throw new Error("useUserContext must be used within a UserProvider");
+  }
+  return context;
+}
 
 export default function UserProvider({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>){
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [user, setUser] = useState<User | null>(null);
 
-    const [user, setUser] = useState<User | null>(null);
-    return(
-        <userContext.Provider value={user}>
-            {children}
-        </userContext.Provider>
-    )
+  const applyUser = (u: User) => {
+    setUser(u); 
+  };
+
+  return (
+    <userContext.Provider value={{ user, applyUser }}>
+      {children}
+    </userContext.Provider>
+  );
 }
