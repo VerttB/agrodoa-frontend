fe915e38c2612126621bba55606a5a90f7ba7a8f
VerttB <alyssonoliveira456@gmail.com>
2025-06-06 10:10:37 -0300

    Página de pagamento

--- src/app/(pages)/(public)/pagamento/page.tsx
+++ src/app/(pages)/(public)/pagamento/page.tsx
@@ -1,3 +1,31 @@
-export default function Pagamento() {
-  return <p>Eai galera</p>;
+import Input from "@/components/input"
+
+interface PagamentoProps{
+  nome: string,
+  quantidade:number,
+  valor: number,
+}
+
+export default function Pagamento({nome,quantidade,valor} : PagamentoProps) {
+  return( 
+    <div className="min-h-screen flex flex-col md:flex-row w-full p-8 gap-8">
+        <div className="flex flex-col w-full gap-8">
+        <h2 className="text-2xl">Dados da Compra</h2>
+        <div className="flex flex-col w-full">
+          <Input label="Produto" className="rounded-xl bg-neutral  border-accent" value={"Maçã"} disabled/>
+          <Input label="Quantidade" className="rounded-xl bg-neutral  border-accent" value={500} disabled/>
+          <Input  label="Preço "  className="rounded-xl bg-neutral  border-accent" value={"R$ 150,00"}disabled/>
+          </div>
+        </div>
+
+        <div className="w-full flex flex-col gap-8">
+            <h2 className="text-2xl">Selecione a forma de pagamento</h2>
+            <div className="w-full flex flex-col">
+            <Input className="rounded-xl bg-neutral border-accent" label="Forma de Pagamento" value={"PIX"} disabled/>
+            <h3 className="py-1">Realize o pagamento utilizando o código pix abauxo,ou, leia o QR code</h3>
+            <Input className="rounded-xl bg-neutral  border-accent" label="Chave Pix" disabled/>
+            </div>
+        </div>
+    </div>
+  )
 }
