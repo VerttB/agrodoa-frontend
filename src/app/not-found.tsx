7c1a3498f6a6707772655e4003ad99f0199d3afb
VerttB <alyssonoliveira456@gmail.com>
2025-06-06 12:22:42 -0300

    botão para voltar á pagina inicial em not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,14 @@
+import Button from "@/components/button";
+import { useRouter } from "next/navigation";
+
 export default function NotFound() {
+  const router = useRouter();
   return (
     <div className="p-10 text-center">
       <h1 className="text-4xl font-bold text-red-500">404</h1>
       <p className="mt-4">Página não encontrada.</p>
+      <Button onClick={() => router.push("/")}>Voltar para página inicial</Button>
+
     </div>
   );
 }
