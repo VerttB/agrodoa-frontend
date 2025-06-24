7d0e5e9b2f30f43b65dcad3d8b57d3bab6cbd62f
verttb <alyssonoliveira456@gmail.com>
2025-06-24 15:32:39 -0300

    refactor: mudancas na forma como pega os params

--- src/app/(pages)/(public)/pagamento/page.tsx
+++ src/app/(pages)/(public)/pagamento/page.tsx
@@ -1,23 +1,19 @@
-"use client";
 
 import { Input } from "@/components/ui/input";
 import { Suspense } from "react";
 
 interface PagamentoProps {
-  searchParams: {
+  searchParams:Promise<{
     id?: string;
-    valor?: string;
+    value?: string;
     name?: string;
-  };
+  }>;
 }
 
-export default function Pagamento({ searchParams }: PagamentoProps) {
-  const itemValues = {
-    itemId: searchParams.id ?? "",
-    value: Number(searchParams.valor) || 0,
-    name: searchParams.name ?? "",
-  };
+export default async function Pagamento({ searchParams }: PagamentoProps) {
 
+  const { id,value,name} = await searchParams
+  console.log(id, value, name)
   return (
     <Suspense>
       <div className="flex min-h-screen w-full flex-col gap-8 p-8 md:flex-row">
@@ -26,20 +22,20 @@ export default function Pagamento({ searchParams }: PagamentoProps) {
           <div className="flex w-full flex-col">
             <Input
               label="Produto"
-              className="bg-neutral border-accent rounded-xl"
-              value={itemValues.name}
+              className="bg-neutral border-accent rounded-xl w-full"
+              value={name}
               disabled
             />
             <Input
               label="Quantidade"
-              className="bg-neutral border-accent rounded-xl"
-              value={itemValues.value}
+              className="bg-neutral border-accent rounded-xl w-full"
+              value={value}
               disabled
             />
             <Input
               label="Preço "
-              className="bg-neutral border-accent rounded-xl"
-              value={`R$: ${itemValues.value}`}
+              className="bg-neutral border-accent rounded-xl w-full"
+              value={`R$: ${value}`}
               disabled
             />
           </div>
@@ -49,7 +45,7 @@ export default function Pagamento({ searchParams }: PagamentoProps) {
           <h2 className="text-2xl">Selecione a forma de pagamento</h2>
           <div className="flex w-full flex-col">
             <Input
-              className="bg-neutral border-accent rounded-xl"
+              className="bg-neutral border-accent rounded-xl w-full"
               label="Forma de Pagamento"
               value={"PIX"}
               disabled
@@ -59,7 +55,7 @@ export default function Pagamento({ searchParams }: PagamentoProps) {
               code
             </h3>
             <Input
-              className="bg-neutral border-accent rounded-xl"
+              className="bg-neutral border-accent rounded-xl w-full"
               label="Chave Pix"
               disabled
             />
