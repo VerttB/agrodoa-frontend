e285244d69592d755e74cadbc2c1eb8bd5b134c6
VerttB <alyssonoliveira456@gmail.com>
2025-06-30 11:40:06 -0300

    feat: usuario teste api

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,23 @@
+import { cookies } from "next/headers";
+
+export async function POST(req: Request) {
+  const mockUser = {
+    id: "1",
+    nome: "Ana Silva",
+    senha: "senha123",
+    email:"ana@gmail.com",
+    cpf_ou_cnpj: "123.456.789-00",
+    tipo: "fornecedor",
+    voluntario: false,
+    telefone: "(71) 99999-1111",
+    cidade: "Salvador",
+  };
+
+  (await cookies()).set("mockUser", JSON.stringify(mockUser), {
+    httpOnly: false, // enquanto é mock, pode ser acessado no client
+    path: "/",
+    maxAge: 60 * 60 * 24,
+  });
+
+  return new Response(JSON.stringify({ success: true }), { status: 200 });
+}
