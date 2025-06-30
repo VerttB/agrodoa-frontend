7a7a8bfc9aeaf778d49c9371faf50c8a21b9ea0c
VerttB <alyssonoliveira456@gmail.com>
2025-06-30 10:55:32 -0300

    feat: pesquisa de anuncio atualizando query

---
+++ src/components/anuncio/AnuncioSearch.tsx
@@ -0,0 +1,49 @@
+'use client';
+ 
+import { Input } from '../ui/input';
+import { Search } from 'lucide-react'; 
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { Button } from '../ui/button';
+import { useEffect, useState } from 'react';
+
+export default function AnuncioSearch() {
+
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    const current = searchParams.get('nome') || '';
+    setSearch(current);
+  }, [searchParams]);
+
+  function handleSearch() {
+    const params = new URLSearchParams();
+    if (search) {
+      params.set('nome', search);
+    } else {
+      params.delete('nome');
+    }
+    console.log(params.toString());
+    console.log(pathname)
+    replace(`${pathname}?${params.toString()}`)
+  }
+ 
+  return (
+  <div className='flex'>
+    <Input
+    className='w-full'
+    placeholder='Insira o nome do anúncio'
+    onChange={(e) => {
+      setSearch(s => e.target.value)
+    }}
+    value={search}
+   
+    />
+    <Button
+      onClick={handleSearch}
+    ><Search></Search></Button>
+    </div>
+  );
+}
