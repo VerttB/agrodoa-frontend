37a2068a88024f7aec2278718e2aaa25584579be
verttb <alyssonoliveira456@gmail.com>
2025-07-10 20:43:16 -0300

    style: Mudando quantidade de anuncios por linha desktop 5 -> 4

--- src/components/anuncio/AnuncioList.tsx
+++ src/components/anuncio/AnuncioList.tsx
@@ -9,7 +9,7 @@ export const AnuncioList = ({ anuncios }: AnuncioListProps) => {
   if (anuncios.length === 0) return <p>Sem anúncios no momento.</p>;
 
   return (
-    <div className="grid w-4/4 gap-2 md:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-5">
+    <div className="grid w-4/4 gap-2 md:grid-cols-2 lg:grid-cols-4 ">
       {anuncios.map((a) => (
         <AnuncioCard key={a.idAnuncio} anuncio={a} />
       ))}
