345297d1ed852a7467ade52a1262b8378458f4a8
verttb <alyssonoliveira456@gmail.com>
2025-07-10 20:29:05 -0300

    fix: Corrigindo rotas para corresponder ao backend

--- src/hooks/useCausa.ts
+++ src/hooks/useCausa.ts
@@ -1,7 +1,7 @@
 import { useFetch } from "./useFetch";
 
 export function useCausa<T = unknown>(id?: string) {
-  const url = id ? `/api/causas/${id}` : `/api/causas`;
+  const url = id ? `http://localhost:8080/causas/${id}` : `http://localhost:8080/causas`;
 
   return useFetch<T>(url);
 }
