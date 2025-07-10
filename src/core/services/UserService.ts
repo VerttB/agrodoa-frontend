3b4def09121d009093035a5b7646de6520dbf688
verttb <alyssonoliveira456@gmail.com>
2025-07-10 20:29:50 -0300

    feat: Funcao para login de usuário

--- src/core/services/UserService.ts
+++ src/core/services/UserService.ts
@@ -2,9 +2,6 @@
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080";
 
 export async function getUsuarios() {
-
-  
- 
   const res = await fetch(`${BASE_URL}/usuarios`, {
     cache: "no-store", 
     headers: {
@@ -17,4 +14,19 @@ export async function getUsuarios() {
   }
 
   return res.json();
+}
+
+export async function loginUsuario(email: string, senha: string) {
+    const res = await fetch(`${BASE_URL}/auth/login`,{
+      method: "POST",
+      headers:{
+        'Content-Type' : 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        senha
+      })
+    })
+
+    if(res.ok) console.log(await res.json())
 }
