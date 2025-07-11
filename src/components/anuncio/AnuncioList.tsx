356fcfbfbb0cd93dfbd19df0312e137d4e470709
verttb <alyssonoliveira456@gmail.com>
2025-07-11 00:46:01 -0300

    refactor: colocando tw merge

--- src/components/anuncio/AnuncioList.tsx
+++ src/components/anuncio/AnuncioList.tsx
@@ -1,15 +1,17 @@
 import { Anuncio } from "@/core/interfaces/Anuncio";
 import { AnuncioCard } from "./AnuncioCard";
+import { twMerge } from "tailwind-merge";
 
 interface AnuncioListProps {
   anuncios: Anuncio[];
+  className?: string,
 }
 
-export const AnuncioList = ({ anuncios }: AnuncioListProps) => {
+export const AnuncioList = ({ anuncios, className }: AnuncioListProps) => {
   if (anuncios.length === 0) return <p>Sem anúncios no momento.</p>;
 
   return (
-    <div className="grid w-4/4 gap-2 md:grid-cols-2 lg:grid-cols-4 ">
+    <div className={twMerge(`grid w-4/4 gap-2 md:grid-cols-2 lg:grid-cols-4`, className)}>
       {anuncios.map((a) => (
         <AnuncioCard key={a.titulo} anuncio={a} />
       ))}
