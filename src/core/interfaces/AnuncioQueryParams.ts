75c10f4a68847d8a5072448dc2b87068b7f37777
verttb <alyssonoliveira456@gmail.com>
2025-07-10 21:52:26 -0300

    refa ctor: trocando arquivos de lugar

--- src/core/interfaces/AnuncioQueryParams.ts
+++
@@ -1,9 +0,0 @@
-export interface AnuncioQueryParams {
-  nome?: string;
-  cidade?: string;
-  precoMin?: number;
-  precoMax?: number;
-  dataExpiracao?: string;
-  tipo?: string;
-  status?: string;
-}
