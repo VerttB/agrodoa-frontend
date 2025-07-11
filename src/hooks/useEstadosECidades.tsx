ff0bed9063f2a9343e965fee05b08a31fcfdd867
verttb <alyssonoliveira456@gmail.com>
2025-07-11 15:15:14 -0300

    feat: finalizado userProvider

---
+++ src/hooks/useEstadosECidades.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { useFetch } from "@/hooks/useFetch";
+
+interface Estado {
+  id: string;
+  nome: string;
+}
+
+interface Cidade {
+  id: string;
+  nome: string;
+}
+
+export function useEstadosECidades() {
+  const [estadoSelecionado, setEstadoSelecionado] = useState<string>("");
+  const [estados, setEstados] = useState<Estado[]>([]);
+  const [cidades, setCidades] = useState<Cidade[]>([]);
+
+  const { data: estadosData } = useFetch<Estado[]>("http://localhost:8080/estados");
+  const { data: cidadesData } = useFetch<Cidade[]>(
+    estadoSelecionado ? `http://localhost:8080/estados/${estadoSelecionado}/cidades` : ""
+  );
+
+
+  useEffect(() => {
+    if (estadosData?.length) {
+      setEstados(estadosData);
+      setEstadoSelecionado(estadosData[0].id); 
+    }
+  }, [estadosData]);
+
+  useEffect(() => {
+    if (cidadesData) {
+      setCidades(cidadesData);
+    } else {
+      setCidades([]); 
+    }
+  }, [cidadesData]);
+
+  return {
+    estados,
+    cidades,
+    estadoSelecionado,
+    setEstadoSelecionado,
+  };
+}
--- src/providers/UserProvider.tsx
+++ src/providers/UserProvider.tsx
@@ -1,15 +1,21 @@
 "use client";
 
 import { Usuario } from "@/core/interfaces/Usuario";
+import { cadastroUsuario, deslogar, loginUsuario, verPerfil } from "@/core/services/UserService";
+import { useRouter } from "next/navigation";
 import { createContext, useState, useContext, useEffect } from "react";
 
 type UserContextType = {
-  user: Usuario | null;
-  login: (user:Usuario) => void
-  logout: () => void
+  user: Usuario | null,
+  isLoading: boolean,
+  isAuthenticated: boolean,
+  login: (email:string, senha:string) => Promise<boolean>,
+  logout: () => void,
+  cadastro: (data: any) => Promise<boolean>
 };
 
 export const UserContext = createContext<UserContextType | null>(null);
+
 export function useUserContext() {
   const context = useContext(UserContext);
   if (!context) {
@@ -24,23 +30,79 @@ export default function UserProvider({
   children: React.ReactNode;
 }) {
   const [user, setUser] = useState<Usuario | null>(null);
-  
-  useEffect(() => {
-    const storedUser = localStorage.getItem("user");
-    if (storedUser) setUser(JSON.parse(storedUser));
-  }, []);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const isAuthenticated = !!user;
+  const router = useRouter();
 
-  const login = (data: Usuario) => {
-    setUser(data);
-    localStorage.setItem("user", JSON.stringify(data));
+  useEffect(() => {
+    const loadUser = async () => {
+      setIsLoading(true);
+      try{
+          const userData = await verPerfil();
+          setUser(userData)
+      }catch(e){
+          console.warn("Nenhum perfil ativo", e);
+          setUser(null);
+      } finally{
+        setIsLoading(false);
+      }
+  }
+  loadUser()
+  },[])
+
+  const login = async (email: string, senha: string):Promise<boolean> => {
+     setIsLoading(true); 
+    try {
+      const userData = await loginUsuario(email, senha); 
+      setUser(userData); 
+      return true; 
+    } catch (error) {
+      console.error("Erro no login:", error);
+      setUser(null); 
+      return false; 
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   const logout = () => {
-    setUser(null);
-    localStorage.removeItem("user");
+    setIsLoading(true);
+
+    try{
+      deslogar();
+      setUser(null)
+      router.push("/");
+    }catch(e){
+      console.error("Erro ao deslogar", e)
+    }finally{
+      setIsLoading(false);
+    }
   };
+
+  const cadastro = async (data:any):Promise<boolean> => {
+    setIsLoading(true);
+    try{
+      const userData = await cadastroUsuario(data)
+      setUser(userData)
+      return true;
+    }catch(e){
+      console.error("Erro no cadastro", e);
+      return false;
+    }finally{
+      setIsLoading(false);
+    }
+  }
+
+  const contextValue = {
+    user,
+    isLoading,
+    isAuthenticated,
+    login,
+    logout,
+    cadastro
+  }
   return (
-    <UserContext.Provider value={{ user, login, logout }}>
+    <UserContext.Provider value={contextValue}>
       {children}
     </UserContext.Provider>
   );
