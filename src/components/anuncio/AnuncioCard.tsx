15446e10c2f1bdc9647829a6ecad0fdf7b4c5513
verttb <alyssonoliveira456@gmail.com>
2025-07-14 14:07:15 -0300

    feat: tooltip salvar anuncio

--- src/components/anuncio/AnuncioCard.tsx
+++ src/components/anuncio/AnuncioCard.tsx
@@ -7,22 +7,30 @@ import { Heart } from "lucide-react";
 import { AnuncioDropdownMenu } from "./AnuncioDropdown";
 import { useUserContext } from "@/providers/UserProvider";
 import { salvarAnuncio } from "@/core/services/AnuncioService";
+import { useState } from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 
 export const AnuncioCard = ({ anuncio }: { anuncio: Anuncio }) => {
   const router = useRouter();
   const { user } = useUserContext();
-  
+  const [sucess,setSucess] = useState(false);
 
   const handleSave = (id: string) => {
     console.log(id)
       try{
         const res = salvarAnuncio(id)
-        console.log(res)
+        router.refresh();
+        setSucess(true);
       }catch(e){
         console.log(e);
       }
   }
   return (
+    <>
     <Card.Root
       key={anuncio.titulo}
       className="relative flex flex-col justify-around hover:scale-105 transition-transform duration-200 shadow-[0px_6px_21px_-6px_rgba(249,_115,_22,_0.5)]"
@@ -38,7 +46,6 @@ export const AnuncioCard = ({ anuncio }: { anuncio: Anuncio }) => {
           <h1 className="mb-4 text-xl font-bold">{anuncio.titulo}</h1>
 
           <p className="text-sm">{anuncio.anunciante.nome}</p>
-          <p className="text-sm">Negociantes:{anuncio.anunciante.nome}</p>
           <p className="text-sm">Preço: {anuncio.produto.precoUnidade ? anuncio.produto.precoUnidade.toLocaleString("pt-BR", { style: "currency", currency:"BRL"}) : "Grátis"}</p>
           <p className="text-sm">Quantidade: {anuncio.produto.quantidade}</p>
         </div>
@@ -52,15 +59,21 @@ export const AnuncioCard = ({ anuncio }: { anuncio: Anuncio }) => {
           Ver Detalhes
         </Button>
         {user?.tipo !== "fornecedor" &&
-        <Button
-          className="flex w-full justify-center gap-1 px-2 py-1"
-          variant="outlined"
-          onClick={() => handleSave(anuncio.idAnuncio)}
-        >
-          Salvar<Heart></Heart>
-        </Button>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+                className="flex w-full justify-center gap-1 px-2 py-1"
+                variant="outlined"
+                onClick={() => handleSave(anuncio.idAnuncio)}
+              >
+            <Heart></Heart>
+            <TooltipContent>Salvar Anúncio</TooltipContent>
+           </Button>
+        </TooltipTrigger>
+        </Tooltip>
 }
       </Card.Actions>
     </Card.Root>
+    </>
   );
 };
