92660a2d27cd7f65176117c82c79806636be0398
verttb <alyssonoliveira456@gmail.com>
2025-07-16 00:03:29 -0300

    feat: descrição no anúncio

--- src/components/anuncio/CriarAnuncioModal.tsx
+++ src/components/anuncio/CriarAnuncioModal.tsx
@@ -12,59 +12,63 @@ import { SelectInput } from "../ui/selectInput";
 import { criarProduto } from "@/core/services/ProdutoService";
 import { criarAnuncio } from "@/core/services/AnuncioService";
 import { useRouter } from "next/navigation";
+import { useEstadosECidades } from "@/hooks/useEstadosECidades";
 
-const createAdSchema = z.object({
-  titulo: z.string().min(3, "Título muito curto").max(100),
-  nome: z.string().min(3, "Nome do produto muito curto").max(100),
-  quantidade: z.coerce
-    .number()
-    .int()
-    .gte(1, "Quantidade mínima 1")
-    .lte(10000, "Quantidade máxima 10000"),
-  preco_unidade: z.coerce.number().gte(0.05, "Preço mínimo R$0,05"),
-
-  data_validade: z.string().refine(
-    (val) => {
-      const date = new Date(val);
-      return date > new Date();
-    },
-    {
-      message: "A data de validade deve ser futura",
-    },
-  ),
-
-  data_expiracao: z.string().refine(
-    (val) => {
-      const date = new Date(val);
-      return date > new Date();
-    },
-    {
-      message: "A data de expiração deve ser futura",
-    },
-  ),
-
-  entrega_pelo_fornecedor: z.enum(["true", "false"], {
-    required_error: "Informe se o fornecedor entrega",
-  }),
-
-  cidade: z.string().min(1, "Informe a cidade"),
-
-  tipoAnuncio: z.enum(["DOACAO", "VENDA"], {
-    required_error: "Informe o tipo do anúncio",
-  }),
-
-  image: z
-    .instanceof(File, { message: "Deve ser uma imagem" })
-    .refine((file) => file?.size <= MAX_FILE_SIZE, "Máximo 5MB")
-    .refine(
-      (file) => ACCEPTED_IMAGE_TYPE.includes(file?.type),
-      "Extensão inválida",
+const createAdSchema = z
+  .object({
+    titulo: z.string().min(3, "Título muito curto").max(100),
+    nome: z.string().min(3, "Nome do produto muito curto").max(100),
+    descricao: z.string().min(10, "Descrição muito curta").max(300),
+    quantidade: z.coerce
+      .number()
+      .int()
+      .gte(1, "Quantidade mínima 1")
+      .lte(10000, "Quantidade máxima 10000"),
+    preco_unidade: z.coerce.number().optional(),
+    data_validade: z.string().refine(
+      (val) => {
+        const date = new Date(val);
+        return date > new Date();
+      },
+      { message: "A data de validade deve ser futura" }
     ),
-});
+    data_expiracao: z.string().refine(
+      (val) => {
+        const date = new Date(val);
+        return date > new Date();
+      },
+      { message: "A data de expiração deve ser futura" }
+    ),
+    entrega_pelo_fornecedor: z.enum(["true", "false"], {
+      required_error: "Informe se o fornecedor entrega",
+    }),
+    cidade: z.string().min(1, "Informe a cidade"),
+    tipoAnuncio: z.enum(["DOACAO", "VENDA"], {
+      required_error: "Informe o tipo do anúncio",
+    }),
+    image: z
+      .instanceof(File, { message: "Deve ser uma imagem" })
+      .refine((file) => file?.size <= MAX_FILE_SIZE, "Máximo 5MB")
+      .refine(
+        (file) => ACCEPTED_IMAGE_TYPE.includes(file?.type),
+        "Extensão inválida"
+      ),
+  })
+  .refine(
+    (data) =>
+      data.tipoAnuncio === "DOACAO" || (data.preco_unidade ?? 0) >= 0.05,
+    {
+      path: ["preco_unidade"],
+      message: "Preço mínimo R$0,05",
+    }
+  );
 
 type CreateAdData = z.infer<typeof createAdSchema>;
 
 export const CriarAnuncio = () => {
+  const { estados, cidades, estadoSelecionado, setEstadoSelecionado } =
+      useEstadosECidades();
+  
   const [open, setOpen] = useState(false);
   const [etapa, setEtapa] = useState(1);
   const router = useRouter();
@@ -75,9 +79,12 @@ export const CriarAnuncio = () => {
     formState: { errors },
     reset,
     trigger,
+    watch,
   } = useForm<CreateAdData>({
     resolver: zodResolver(createAdSchema),
   });
+
+  const tipoAnuncio = watch("tipoAnuncio");
 
   const onSubmit = async (data: CreateAdData) => {
     let idAnuncio = 0;
@@ -85,8 +92,9 @@ export const CriarAnuncio = () => {
       nome: data.nome,
       quantidade: data.quantidade,
       dataValidade: data.data_validade,
-      precoUnidade: data.preco_unidade,
+      precoUnidade: data.tipoAnuncio === "DOACAO" ? 0 : data.preco_unidade,
     };
+
     try {
       const res = await criarProduto(produto);
       if (res.idProduto) idAnuncio = res.idProduto;
@@ -94,15 +102,18 @@ export const CriarAnuncio = () => {
       console.error("Erro ao criar produto:", e);
     }
 
-   const formData = new FormData();
-
-formData.append("titulo", data.titulo);
-formData.append("imagem", data.image); 
-formData.append("entregaPeloFornecedor", data.entrega_pelo_fornecedor ? "1" : "0"); 
-formData.append("tipoAnuncio", data.tipoAnuncio);
-formData.append("cidadeId", data.cidade); 
-formData.append("produtoId", String(idAnuncio));
-formData.append("dataExpiracao", data.data_expiracao);
+    const formData = new FormData();
+    formData.append("titulo", data.titulo);
+    formData.append("imagem", data.image);
+    formData.append(
+      "entregaPeloFornecedor",
+      data.entrega_pelo_fornecedor === "true" ? "1" : "0"
+    );
+    formData.append("tipoAnuncio", data.tipoAnuncio);
+    formData.append("cidadeId", data.cidade);
+    formData.append("produtoId", String(idAnuncio));
+    formData.append("dataExpiracao", data.data_expiracao);
+    formData.append("descricao", data.descricao);
 
     try {
       const res = await criarAnuncio(formData);
@@ -145,49 +156,50 @@ formData.append("dataExpiracao", data.data_expiracao);
                   errors={errors.titulo?.message}
                   className="w-full"
                 />
-                <Input
-                  label="Cidade"
-                  placeholder="Ex: Salvador"
-                  {...register("cidade")}
-                  errors={errors.cidade?.message}
-                  className="w-full"
+               <div className="flex w-full gap-2">
+                  <SelectInput
+                   
+                    data={estados.map((e) => ({ value: e.id, text: e.nome }))}
+                    onChange={(e) => {
+                      setEstadoSelecionado(e.target.value);
+                    
+                    }}
+                    label="Estado"
+                    placeholder="Ex: Bahia"
+                    className="flex-1 bg-white py-2"
+                  />
+                  <SelectInput
+                    {...register("cidade")}
+                    data={
+                      cidades?.map((c) => ({ value: c.id, text: c.nome })) ?? []
+                    }
+                    disabled={!estadoSelecionado}
+                    label="Cidade"
+                    errors={errors.cidade?.message}
+                    placeholder="Selecione a cidade"
+                    className="flex-1 bg-white py-2"
+                  />
+                </div>
+                <SelectInput
+                  label="Entrega pelo Fornecedor"
+                  data={[
+                    { value: "true", text: "Sim" },
+                    { value: "false", text: "Não" },
+                  ]}
+                  className="mt-1 w-full"
+                  {...register("entrega_pelo_fornecedor")}
+                  errors={errors.entrega_pelo_fornecedor?.message}
                 />
-                <div>
-                  <label className="text-sm font-medium">
-                    Entrega pelo Fornecedor
-                  </label>
-                  <SelectInput
-                    data={[
-                      { value: "true", text: "Sim" },
-                      { value: "false", text: "Não" },
-                    ]}
-                    className="mt-1 w-full rounded border border-gray-300 bg-white p-2"
-                    {...register("entrega_pelo_fornecedor")}
-                  />
-                  {errors.entrega_pelo_fornecedor && (
-                    <p className="text-sm text-red-500">
-                      {errors.entrega_pelo_fornecedor.message}
-                    </p>
-                  )}
-                </div>
-
-                <div>
-                  <label className="text-sm font-medium">Tipo do Anúncio</label>
-                  <SelectInput
-                    data={[
-                      { value: "DOACAO", text: "Doação" },
-                      { value: "VENDA", text: "Venda" },
-                    ]}
-                    className="mt-1 w-full rounded border border-gray-300 bg-white p-2"
-                    {...register("tipoAnuncio")}
-                  />
-                  {errors.tipoAnuncio && (
-                    <p className="text-sm text-red-500">
-                      {errors.tipoAnuncio.message}
-                    </p>
-                  )}
-                </div>
-
+                <SelectInput
+                  label="Tipo do Anúncio"
+                  data={[
+                    { value: "DOACAO", text: "Doação" },
+                    { value: "VENDA", text: "Venda" },
+                  ]}
+                  className="mt-1 w-full"
+                  {...register("tipoAnuncio")}
+                  errors={errors.tipoAnuncio?.message}
+                />
                 <Input
                   label="Data de Expiração"
                   type="date"
@@ -207,6 +219,16 @@ formData.append("dataExpiracao", data.data_expiracao);
                   errors={errors.nome?.message}
                   className="w-full"
                 />
+                <textarea
+                  placeholder="Descrição do produto"
+                  {...register("descricao")}
+                  className="w-full rounded border border-gray-300 p-2 text-sm"
+                />
+                {errors.descricao && (
+                  <p className="text-sm text-red-500">
+                    {errors.descricao.message}
+                  </p>
+                )}
                 <Input
                   label="Quantidade"
                   type="number"
@@ -214,14 +236,16 @@ formData.append("dataExpiracao", data.data_expiracao);
                   errors={errors.quantidade?.message}
                   className="w-full"
                 />
-                <Input
-                  label="Preço por Unidade"
-                  type="number"
-                  {...register("preco_unidade")}
-                  step={0.01}
-                  errors={errors.preco_unidade?.message}
-                  className="w-full"
-                />
+                {tipoAnuncio === "VENDA" && (
+                  <Input
+                    label="Preço por Unidade"
+                    type="number"
+                    {...register("preco_unidade")}
+                    step={0.01}
+                    errors={errors.preco_unidade?.message}
+                    className="w-full"
+                  />
+                )}
                 <Input
                   label="Data de Validade"
                   type="date"
@@ -244,7 +268,6 @@ formData.append("dataExpiracao", data.data_expiracao);
                 />
               </>
             )}
-
             <Modal.Actions>
               {etapa > 1 && (
                 <Button
